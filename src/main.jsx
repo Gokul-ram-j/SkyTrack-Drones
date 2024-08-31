d6aa7c9ad461f73555022194425ae7236392a02e
gokul-ram-j <gokulram221@gmail.com>
2024-08-31 09:16:42 +0530

    package upd

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/SkyTrack-Drones">
       <App />
     </BrowserRouter>
   </StrictMode>
