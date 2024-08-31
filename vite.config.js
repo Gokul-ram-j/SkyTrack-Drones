b5ed08c304bb6e754a571e64dec96b221721d082
gokul-ram-j <gokulram221@gmail.com>
2024-08-31 10:03:19 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/SkyTrack-Drones/',
+  build: {
+    outDir: 'dist', // Ensure this is where the build output is stored
+  },
 })
 
